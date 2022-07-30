41d4df126595aff8aa793e6d2939e1dde573a2a2
Rafael Botelho <rafaelbsdesigner@gmail.com>
2022-07-30 11:18:12 -0300

    criei novos exercicios javascript

---
+++ aula 16/function01.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return `Impar!`
+    }
+}
+
+let res = parimpar(223);
+console.log(res);
+
+// ou console.log(parimpar(4));
---
+++ aula 16/function02.js
@@ -0,0 +1,7 @@
+function soma(n1,n2) {
+    return n1 + n2
+}
+
+console.log(soma(2,5));
+
+//da pra fazer assim => function soma(n1=4,n2=4);
